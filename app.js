21f3cdc67fc1b5787b22ae34e5654a34d8bef2df
greatGOSVi <osorno.gabo@gmail.com>
2021-05-17 19:44:33 -0500

    Summoner Search 1.0

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const fetch = require("node-fetch");
 const api = express();
 api.use(cors());
 
-api.get("/summoner-info", (req, res) => {
-    //req.query.region
-    res.send({data: "hey"});
+const apiKey = "RGAPI-35a09d36-de8d-4b1b-8137-32534adfd528";
+
+api.get("/summoner-info", async (req, res) => {
+    const summInfoResponse = await fetch(`https://${req.query.region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.query.sumName}?api_key=${apiKey}`);
+    const summInfo = await summInfoResponse.json();
+    res.send(summInfo);
 });
 
-api.get("/champions", (req, res) => {
-    res.send("lo que quieras, champions");
+api.get("/summoner-league", async (req, res) => {
+    const summLeagueResponse = await fetch(`https://${req.query.region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${req.query.sumID}?api_key=${apiKey}`);
+    const summLeague = await summLeagueResponse.json();
+    res.send(summLeague);
 });
 
 api.listen(3000, () => {
     console.log("Open Port 3000");
-})
+});
